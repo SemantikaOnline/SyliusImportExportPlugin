229babbe93a19a9139dad898220b83712356343e
Krzysztof Oleszkiewicz <oleszkiewicz.k@gmail.com>
2020-07-15 18:41:13 +0200

    added payments exporter

---
+++ src/Exporter/Plugin/PaymentResourcePlugin.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FriendsOfSylius\SyliusImportExportPlugin\Exporter\Plugin;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sylius\Component\Core\Model\PaymentInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+
+class PaymentResourcePlugin extends ResourcePlugin
+{
+    public function __construct(
+        RepositoryInterface $repository,
+        PropertyAccessorInterface $propertyAccessor,
+        EntityManagerInterface $entityManager
+    ) {
+        parent::__construct($repository, $propertyAccessor, $entityManager);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init(array $idsToExport): void
+    {
+        parent::init($idsToExport);
+
+        /** @var PaymentInterface $resource */
+        foreach ($this->resources as $resource) {
+            $this->addGeneralData($resource);
+            $this->addOrderData($resource);
+        }
+    }
+
+    private function addGeneralData(PaymentInterface $resource): void
+    {
+        $this->addDataForResource($resource, 'Payment_date', 'test');
+    }
+
+    private function addOrderData(PaymentInterface $resource): void
+    {
+        $this->addDataForResource($resource, 'Order_number', '1');
+        $this->addDataForResource($resource, 'Order_email', '2');
+    }
+}
